583db884120be090c8eab757ec51b1b32eeac035
iwwenbo <iwwenbo@gmail.com>
2016-04-30 00:21:58 +0800

    cur tag page

---
+++ curtag.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="zh-cn">
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<link rel="stylesheet" type="text/css" href="resources/bootstrap/css/bootstrap.min.css" media="screen">
+	<link rel="stylesheet" type="text/css" href="resources/css/lehudy.css" media="screen">
+	<script type="text/javascript" src="resources/js/jquery-1.12.2.min.js"></script>
+	<script type="text/javascript" src="resources/bootstrap/js/bootstrap.min.js"></script>
+	<title>乐乎电影标签：喜剧</title>
+</head>
+<body>
+<nav class="navbar navbar-default latest-navbar"> 
+	<div class="container-fluid"> 
+		<div class="navbar-header latest-navbar-header"> 
+			<!-- 移动版按钮 -->
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-2" aria-expanded="false"> 
+				<span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> 
+				<span class="icon-bar"></span> 
+				<span class="icon-bar"></span> 
+			</button> 
+			<a class="latest-navbar-brand" href="/movies/home.php">乐乎电影</a> 
+		</div> 
+		<div class="collapse navbar-collapse latest-navbar-search" id="bs-example-navbar-collapse-2"> 
+			<div class="input-group btn-group latest-search-group" style="width:485px;">
+                <input type="text" class="form-control dropdown-toggle" style="width:450px;" id="sn" placeholder="影视名称">
+                <span class="input-group-btn">
+                    <button type="button" class="btn btn-danger" id="searchbutton" style="height:34px;">搜索电影</button>
+                </span>
+            </div>
+		</div> 
+	</div> 
+	<div class="nav-secondary">
+      <div class="nav-items nav-anon">
+          <ul>
+              <li><a href="https://movie.douban.com/nowplaying/">爱情</a></li>
+              <li><a href="https://movie.douban.com/explore">动作</a></li>
+              <li><a href="https://movie.douban.com/tv/">战争</a></li>
+              <li><a href="https://movie.douban.com/chart">科幻</a></li>
+              <li><a href="https://movie.douban.com/tag/">悬疑</a></li>
+              <li><a href="https://movie.douban.com/review/best/">恐怖</a></li>
+              <li><a href="https://movie.douban.com/annual2015/">喜剧</a></li>
+              <li><a href="https://movie.douban.com/annual2015/">惊悚</a></li>
+              <li><a href="https://movie.douban.com/annual2015/">动画</a></li>
+              <li><a href="https://movie.douban.com/annual2015/">冒险</a></li>
+              <li><a href="https://movie.douban.com/annual2015/">电视剧</a></li>
+              <li><a href="https://movie.douban.com/standbyme/?source=navigation">更多...</a></li>
+          </ul>
+      </div>
+  </div>
+</nav>
+<div id="wrapper">
+	<div id="content">
+        <div class="article">
+	        <h1>最新电影</h1>
+			<div class="list-curtag">
+		    <div class="list">
+		    	<div class="">
+		    		<p class="ul first"></p>
+		    		<table width="100%" class="">
+				        <tbody><tr class="item">
+				            <td width="100" valign="top">
+				                    <a class="nbg" href="https://movie.douban.com/subject/25662329/" title="疯狂动物城">
+				                        <img src="https://img1.doubanio.com/view/movie_poster_cover/ipst/public/p2315672647.jpg" alt="疯狂动物城" class="">
+				                    </a>
+				            </td>
+				            <td valign="top">
+				                <div class="pl2">
+				                    <a href="https://movie.douban.com/subject/25662329/" class="">
+				                        疯狂动物城
+				                        / <span style="font-size:12px;">优兽大都会(港) / 动物方城市(台)</span>
+				                    </a>
+				                    <p class="pl">2016-03-04(中国大陆/美国) / 金妮弗·古德温 / 杰森·贝特曼 / 伊德里斯·艾尔巴 / 珍妮·斯蕾特 / 内特·托伦斯 / 邦尼·亨特 / 唐·雷克 / 汤米·钟 / J·K·西蒙斯 / 奥克塔维亚·斯宾瑟 / 艾伦·图代克 / 夏奇拉 / 雷蒙德·S·佩尔西 / 德拉·萨巴...</p>
+				                        <div class="star clearfix">
+				                                    <span class="allstar50"></span>
+				                                    <span class="rating_nums">9.3</span>
+				                            <span class="pl">(280207人评价)</span>
+				                        </div>
+				                </div>
+				            </td>
+				        </tr>
+				    </tbody>
+    			</table>
+		    	</div>
+		    </div>
+    		<a class="more" href="javascript:;">加载更多</a>
+		</div> <!-- end list-mv -->
+        </div> <!-- end article -->
+        
+        <div class="aside">
+        	<div id="hotlist">
+		        <h2>热门推荐</h2>
+		        <ul>
+		            <li>
+		            <span>15推荐</span>
+		            <div class="title"><a target="_blank" href="https://www.douban.com/doulist/1691871/">【总】100部必看的经典爱情电影</a></div>
+		            </li>
+		            <li>
+		            <span>4757推荐</span>
+		            <div class="title"><a target="_blank" href="https://www.douban.com/doulist/119320/">BBC古典名著小剧集(BBC CLASSIC DRAMA)</a></div>
+		            </li>
+		            <li>
+		            <span>141推荐</span>
+		            <div class="title"><a target="_blank" href="https://www.douban.com/doulist/256745/">香港电影评论学会大奖历届获奖名单</a></div>
+		            </li>
+		            <li>
+		            <span>231推荐</span>
+		            <div class="title"><a target="_blank" href="https://www.douban.com/doulist/769172/">我所看过的中国独立电影</a></div>
+		            </li>
+		            <li>
+		            <span>7推荐</span>
+		            <div class="title"><a target="_blank" href="https://www.douban.com/doulist/2530328/">电影中的冷知识</a></div>
+		            </li>
+		        </ul>
+        	</div>
+        </div>
+	</div>
+	<div id="footer">
+            <div class="footer-extra"></div>
+			<span id="icp" class="fleft gray-link">
+			    © 2015－2016 lehudy.com, all rights reserved
+			</span>
+			<span class="fright">
+		    	<a href="https://www.lehudy.com/">关于乐乎电影</a>
+		    </span>
+     </div>
+</div>
+</body>
+</html>
